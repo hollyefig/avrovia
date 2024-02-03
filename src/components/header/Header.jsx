1dc856993163df42cb5018d540088848056a021a
“Holly <hollyefig@gmail.com>
2024-02-02 21:12:43 -0500

    header added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className='headerWrap'>
+      <div className='subheadWrap'>
+        <div className='headerSpacer'></div>
+        <div className='subhead'>a DnD Podcast</div>
+      </div>
+    </div>
+  );
+}
