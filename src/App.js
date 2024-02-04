b4dc5fe0b19d5ea7383c5114b22b552c4d77963b
“Holly <hollyefig@gmail.com>
2024-02-03 23:05:20 -0500

    updated card set for mobile, horizontal scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./components/header/Header";
 import Sect1 from "./components/sect1/Sect1";
 import Sect2 from "./components/sect2/Sect2";
@@ -8,19 +8,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-  // useEffect(() => {
-  //   gsap.from(".charCardsWrap", {
-  //     opacity: 0,
-  //     duration: 1,
-  //     scrollTrigger: {
-  //       trigger: ".charCardsWrap",
-  //       start: "top 80%", // Adjust the start position
-  //       end: "bottom 20%", // Adjust the end position
-  //       scrub: true,
-  //     },
-  //   });
-  // }, []);
-
   return (
     <div className='App'>
       <Header />
